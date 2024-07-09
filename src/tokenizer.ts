3cb5a5d9c3c1413fac120100cca90b3a72529337
Stark <inventorstark7@gmail.com>
2024-07-09 19:24:38 +0100

    feat: tokenizer completed

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,3 +1,5 @@
+import { isFalse, isNull, isNumber, isTrue } from "./utils";
+
 export type TokenType =
   | "OPENBRACE"
   | "CLOSEBRACE"
@@ -8,30 +10,117 @@ export type TokenType =
   | "NULL"
   | "COMMA"
   | "COLON"
+  | "OPENQUOTE"
+  | "CLOSEQUOTE"
   | "STRING"
-  | "Number";
+  | "NUMBER";
+
+const OPENBRACE = "{";
+const CLOSEBRACE = "}";
+const OPENBRACKET = "[";
+const CLOSEBRACKET = "]";
+const COLON = ":";
+const COMMA = ",";
+const TRUE = "true";
+const FALSE = "false";
+const NULL = "null";
+const OPENQUOTE = '"';
+const CLOSEQUOTE = '"';
+// considering best way to represent it
+// might as well have a validation check
+// const NUMBER = ""
 
 interface Token {
-    type: TokenType
-    value: string
-  }
-
+  type: TokenType;
+  value: string;
+}
 
 /**
- * 
+ *
  * @param input - a string to parse as json
  * @returns returns an array of tokens - `Token[]`
  */
 export const tokenizer = (input: any): Token[] => {
-    // keep track of current character
-    let current = 0
+  let tokens: Token[] = [];
+  // keep track of current character
+  let current = 0;
 
-    // loop through each char of input
-    while (current < input.length) {
-    let char = input[current]
+  // loop through each char of input
+  while (current < input.length) {
+    let char = input[current];
 
-        current++
+    switch (char) {
+      case OPENBRACE:
+        tokens.push({ type: "OPENBRACE", value: char });
+        current++;
+        continue;
+
+      case CLOSEBRACE:
+        tokens.push({ type: "CLOSEBRACE", value: char });
+        current++;
+        continue;
+
+      case OPENBRACKET:
+        tokens.push({ type: "OPENBRACKET", value: char });
+        current++;
+        continue;
+
+      case CLOSEBRACKET:
+        tokens.push({ type: "CLOSEBRACKET", value: char });
+        current++;
+        continue;
+      case COLON:
+        tokens.push({ type: "COLON", value: char });
+        current++;
+        continue;
+
+      case COMMA:
+        tokens.push({ type: "COMMA", value: char });
+        current++;
+        continue;
+      // handling string
+      case OPENQUOTE:
+        tokens.push({ type: "OPENQUOTE", value: char });
+        let value = "";
+        char = input[++current];
+
+        while (char != CLOSEQUOTE) {
+          value += char;
+          char = input[++current];
+        }
+        current++;
+        tokens.push({ type: "STRING", value: value });
+        tokens.push({ type: "CLOSEQUOTE", value: CLOSEQUOTE });
+        continue;
+
+      default:
+        // handle numbers, null, booleans
+        // perform regex test
+        if (/[\d\w]/.test(char)) {
+          let value = "";
+          while (/[\d\w]/.test(char)) {
+            value += char;
+            char = input[++current];
+          }
+
+          // check if token is number, boolean
+          if (isNumber(value)) tokens.push({ type: "NUMBER", value: value });
+          else if (isTrue(value)) tokens.push({ type: "TRUE", value: value });
+          else if (isFalse(value)) tokens.push({ type: "FALSE", value: value });
+          else if (isNull(value)) tokens.push({ type: "NULL", value: value });
+          //   handles generic case of unhandle values
+          else throw new Error(`[Unexpected value]:  ${value}`);
+
+          continue;
+        }
+        // skip whitespace
+        if (/\s/.test(char)) {
+          current++;
+          continue;
+        }
+
+        throw new Error(`[Invalid token]: ${char}`);
     }
-    // placeholder
-    return {} as Token[]
   }
+  return tokens;
+};
